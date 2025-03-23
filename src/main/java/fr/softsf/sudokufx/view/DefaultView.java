bbfa6cb4c8351d8693e216fc1457230d9298077a
Lob2018 <@>
2025-03-23 18:56:04 +0100

    Feat default-view.fxml Default-view : - Initialize values in Default-view - Moved from some Text to Label to improve formatting - Accessibility role description :     # MENU BUTTONS ACCESSIBILITY STATUS     menu.accessibility.role.description.selected=This button is selected.     menu.accessibility.role.description.opened=This submenu is open.     menu.accessibility.role.description.closed=This submenu is closed.     menu.accessibility.role.description.submenu.option=Submenu option.

--- src/main/java/fr/softsf/sudokufx/view/DefaultView.java
+++ src/main/java/fr/softsf/sudokufx/view/DefaultView.java
@@ -1,10 +1,12 @@
 package fr.softsf.sudokufx.view;
 
 import fr.softsf.sudokufx.SudoMain;
+import fr.softsf.sudokufx.enums.I18n;
 import fr.softsf.sudokufx.enums.Paths;
 import fr.softsf.sudokufx.interfaces.IMainView;
 import fr.softsf.sudokufx.interfaces.ISceneProvider;
 import fr.softsf.sudokufx.interfaces.ISplashScreenView;
+import fr.softsf.sudokufx.view.components.toaster.ToasterVBox;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
@@ -13,11 +15,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Objects;
 
@@ -31,13 +35,141 @@ public final class DefaultView implements IMainView, ISceneProvider {
     private static final double FADE_IN_IN_SECONDS_AFTER_SPLASHSCREEN = 0.3;
     private final Stage primaryStage = new Stage();
     @FXML
-    private Label welcomeText;
-    @FXML
-    private Label version;
-    @FXML
-    private Label githubVersion;
+    @Autowired
+    private ToasterVBox toaster;
     @FXML
     private Button menuHiddenButtonShow;
+    @FXML
+    private Button menuMiniButtonShow;
+    @FXML
+    private Button menuMiniButtonPlayer;
+    @FXML
+    private Button menuMiniButtonEasy;
+    @FXML
+    private Button menuMiniButtonMedium;
+    @FXML
+    private Button menuMiniButtonDifficult;
+    @FXML
+    private Button menuMiniButtonSolve;
+    @FXML
+    private Button menuMiniButtonBackup;
+    @FXML
+    private Button menuMiniButtonBackground;
+    @FXML
+    private Button menuMiniButtonLanguage;
+    @FXML
+    private Label menuMiniButtonLanguageIso;
+    @FXML
+    private Button menuMiniButtonHelp;
+    @FXML
+    private Button menuMiniButtonNew;
+
+    @FXML
+    private Button menuMaxiButtonReduce;
+    @FXML
+    private Label menuMaxiButtonReduceText;
+    @FXML
+    private Button menuMaxiButtonPlayer;
+    @FXML
+    private Label menuMaxiButtonPlayerText;
+    @FXML
+    private Button menuMaxiButtonEasy;
+    @FXML
+    private Label menuMaxiButtonEasyText;
+    @FXML
+    private Button menuMaxiButtonMedium;
+    @FXML
+    private Label menuMaxiButtonMediumText;
+    @FXML
+    private Button menuMaxiButtonDifficult;
+    @FXML
+    private Label menuMaxiButtonDifficultText;
+    @FXML
+    private Button menuMaxiButtonSolve;
+    @FXML
+    private Label menuMaxiButtonSolveText;
+    @FXML
+    private Button menuMaxiButtonSolveClear;
+    @FXML
+    private Button menuMaxiButtonBackup;
+    @FXML
+    private Label menuMaxiButtonBackuptext;
+    @FXML
+    private Button menuMaxiButtonBackground;
+    @FXML
+    private Label menuMaxiButtonBackgroundText;
+    @FXML
+    private Button menuMaxiButtonLanguage;
+    @FXML
+    private Label menuMaxiButtonLanguageIso;
+    @FXML
+    private Label menuMaxiButtonLanguageText;
+    @FXML
+    private Button menuMaxiButtonHelp;
+    @FXML
+    private Label menuMaxiButtonHelpText;
+    @FXML
+    private Button menuMaxiButtonNew;
+    @FXML
+    private Label menuMaxiButtonNewText;
+
+
+    /**
+     * Initializes the default view. This method is automatically called by
+     * JavaFX after loading the FXML.
+     */
+    @FXML
+    private void initialize() {
+        menuHiddenButtonShow.setAccessibleText(I18n.INSTANCE.getValue("menu.hidden.button.show.accessibility"));
+        menuMiniButtonShow.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.show.accessibility"));
+        menuMiniButtonPlayer.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.player.accessibility"));
+        menuMiniButtonPlayer.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMiniButtonEasy.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.easy.accessibility"));
+        menuMiniButtonMedium.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.medium.accessibility"));
+        menuMiniButtonDifficult.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.difficult.accessibility"));
+        menuMiniButtonSolve.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.solve.accessibility"));
+        menuMiniButtonSolve.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMiniButtonBackup.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.backup.accessibility"));
+        menuMiniButtonBackup.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMiniButtonBackground.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.background.accessibility"));
+        menuMiniButtonBackground.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMiniButtonLanguage.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.language.accessibility"));
+        menuMiniButtonLanguageIso.setText(I18n.INSTANCE.getValue("menu.mini.button.language.iso"));
+        menuMiniButtonHelp.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.help.accessibility"));
+        menuMiniButtonNew.setAccessibleText(I18n.INSTANCE.getValue("menu.mini.button.new.accessibility"));
+
+        menuMaxiButtonReduce.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.reduce.accessibility"));
+        menuMaxiButtonReduceText.setText(I18n.INSTANCE.getValue("menu.maxi.button.reduce.text"));
+        menuMaxiButtonPlayer.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.player.accessibility"));
+        menuMaxiButtonPlayer.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        // service is needed
+        menuMaxiButtonPlayerText.setText("PLAYERPLAYERPLAYERPLAYERPLAYERPLAYERPLAYERPLAYER");
+        menuMaxiButtonEasy.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.easy.accessibility"));
+        menuMaxiButtonEasyText.setText(I18n.INSTANCE.getValue("menu.maxi.button.easy.text"));
+        menuMaxiButtonMedium.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.medium.accessibility"));
+        menuMaxiButtonMediumText.setText(I18n.INSTANCE.getValue("menu.maxi.button.medium.text"));
+        menuMaxiButtonDifficult.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.difficult.accessibility"));
+        menuMaxiButtonDifficultText.setText(I18n.INSTANCE.getValue("menu.maxi.button.difficult.text"));
+        menuMaxiButtonSolve.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.solve.accessibility"));
+        menuMaxiButtonSolve.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMaxiButtonSolveText.setText(I18n.INSTANCE.getValue("menu.maxi.button.solve.text"));
+        menuMaxiButtonSolveClear.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.solve.clear.accessibility"));
+        menuMaxiButtonSolveClear.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.submenu.option"));
+        menuMaxiButtonBackup.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.backup.accessibility"));
+        menuMaxiButtonBackup.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMaxiButtonBackuptext.setText(I18n.INSTANCE.getValue("menu.maxi.button.backup.text"));
+        menuMaxiButtonBackground.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.background.accessibility"));
+        menuMaxiButtonBackground.setAccessibleRoleDescription(I18n.INSTANCE.getValue("menu.accessibility.role.description.closed"));
+        menuMaxiButtonBackgroundText.setText(I18n.INSTANCE.getValue("menu.maxi.button.background.text"));
+        menuMaxiButtonLanguage.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.language.accessibility"));
+        menuMaxiButtonLanguageIso.setText(I18n.INSTANCE.getValue("menu.maxi.button.language.iso"));
+        menuMaxiButtonLanguageText.setText(I18n.INSTANCE.getValue("menu.maxi.button.language.text"));
+        menuMaxiButtonHelp.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.help.accessibility"));
+        menuMaxiButtonHelpText.setText(I18n.INSTANCE.getValue("menu.maxi.button.help.text"));
+        menuMaxiButtonNew.setAccessibleText(I18n.INSTANCE.getValue("menu.maxi.button.new.accessibility"));
+        menuMaxiButtonNewText.setText(I18n.INSTANCE.getValue("menu.maxi.button.new.text"));
+
+    }
 
     /**
      * Configures the primary stage for the full menu view.
