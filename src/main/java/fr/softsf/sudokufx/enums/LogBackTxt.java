c9a3e4a91b91a89cef384b12f069aff2f3dcaaa8
Lob2018 <@>
2025-05-28 23:19:44 +0200

    ♻️ refactor MyLogbackConfig:Adds a line break before ▓ Application entry ▓ message

--- src/main/java/fr/softsf/sudokufx/enums/LogBackTxt.java
+++ src/main/java/fr/softsf/sudokufx/enums/LogBackTxt.java
@@ -15,6 +15,7 @@ public enum LogBackTxt {
             """),
     ASCII_LOGO(
             """
+
             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Application entry ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
                      ▒▒▒▒▒                                                                                     \s
