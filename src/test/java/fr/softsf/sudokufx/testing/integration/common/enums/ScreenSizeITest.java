04e24880c2d726dd0b6341a54b0d37a55d6cdfd6
Lob2018 <@>
2025-06-25 11:57:13 +0200

    ✅ test : rename MyEnumsITest to ScreenSizeITest

--- src/test/java/fr/softsf/sudokufx/testing/integration/common/enums/ScreenSizeITest.java
+++ src/test/java/fr/softsf/sudokufx/testing/integration/common/enums/ScreenSizeITest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ApplicationExtension.class)
-class MyEnumsITest {
+class ScreenSizeITest {
 
     @Start
     private void start(Stage primarystage) {
