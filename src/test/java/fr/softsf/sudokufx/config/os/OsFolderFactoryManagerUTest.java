45079ded42f84e07543dc6de9605d906fb562a09
Lob2018 <@>
2025-06-26 12:23:27 +0200

    ✅ test OsFolderFactoryManagerUTest: update OsFolderFactoryManagerUTest to use OsName enum and improve invalid OS tests

--- src/test/java/fr/softsf/sudokufx/config/os/OsFolderFactoryManagerUTest.java
+++ src/test/java/fr/softsf/sudokufx/config/os/OsFolderFactoryManagerUTest.java
@@ -13,14 +13,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import fr.softsf.sudokufx.common.enums.OsName;
+
+import static fr.softsf.sudokufx.common.enums.OsName.EMPTY_OS_FOR_TESTS;
+import static fr.softsf.sudokufx.common.enums.OsName.WRONG_OS_FOR_TESTS;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OsFolderFactoryManagerUTest {
     private OsFolderFactoryManager osFolderFactoryManager;
     private IOsFolderFactory iCurrentIOsFolderFactory;
 
-    static Stream<String> provideInvalidOperatingSystems() {
-        return Stream.of(null, "");
+    static Stream<OsName> provideInvalidOperatingSystems() {
+        return Stream.of(EMPTY_OS_FOR_TESTS, WRONG_OS_FOR_TESTS);
     }
 
     @BeforeEach
@@ -42,16 +46,16 @@ class OsFolderFactoryManagerUTest {
 
     @ParameterizedTest
     @MethodSource("provideInvalidOperatingSystems")
-    void givenNullOrEmptyOs_whenCreateOsFolderFactory_thenIllegalArgumentExceptionTh(String os) {
-        if (os == null) {
-            osFolderFactoryManager.setNullOsForTests();
-        } else if (os.isEmpty()) {
+    void givenNullOrEmptyOs_whenCreateOsFolderFactory_thenIllegalArgumentExceptionTh(OsName os) {
+        if (os == EMPTY_OS_FOR_TESTS) {
             osFolderFactoryManager.setEmptyOsForTests();
+        } else if (os == WRONG_OS_FOR_TESTS) {
+            osFolderFactoryManager.setWrongOsForTests();
         }
         IllegalArgumentException exception =
                 assertThrows(
                         IllegalArgumentException.class, osFolderFactoryManager::iOsFolderFactory);
-        assertTrue(exception.getMessage().contains("Operating system is not specified or null."));
+        assertTrue(exception.getMessage().contains("Unsupported OS: " + os));
     }
 
     @ParameterizedTest
