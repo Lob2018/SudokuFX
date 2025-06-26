09406b4d4470e9fd62cffd8b6b1574beb0f58d61
Lob2018 <@>
2025-06-26 13:54:57 +0200

    ✨ feat HttpClientConfig: switch HttpClient SSLContext from TLSv1.2 to TLSv1.3

--- src/main/java/fr/softsf/sudokufx/config/HttpClientConfig.java
+++ src/main/java/fr/softsf/sudokufx/config/HttpClientConfig.java
@@ -16,21 +16,21 @@ import org.springframework.context.annotation.Configuration;
 
 import fr.softsf.sudokufx.common.annotation.ExcludedFromCoverageReportGenerated;
 
-/** Configures a secure HttpClient bean with TLS 1.2, no redirects, and a 5-second timeout. */
+/** Configures a secure HttpClient bean with TLS 1.3, no redirects, and a 5-second timeout. */
 @Configuration
 @ExcludedFromCoverageReportGenerated
 public class HttpClientConfig {
 
     /**
-     * Provides a secure HttpClient with TLS 1.2.
+     * Provides a secure HttpClient with TLS 1.3.
      *
      * @return Configured HttpClient instance.
-     * @throws NoSuchAlgorithmException if TLS 1.2 is unsupported.
+     * @throws NoSuchAlgorithmException if TLS 1.3 is unsupported.
      * @throws KeyManagementException if SSL context initialization fails.
      */
     @Bean
     HttpClient httpClient() throws KeyManagementException, NoSuchAlgorithmException {
-        final SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        final SSLContext sslContext = SSLContext.getInstance("TLSv1.3");
         sslContext.init(null, null, null);
         return HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NEVER)
