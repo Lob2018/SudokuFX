9374b64492dcc19057efef22784d97b4b7109538
Lob2018 <@>
2025-08-16 19:30:50 +0200

    📝 style MenuNewViewModelUTest: run automatic code formatter

--- src/test/java/fr/softsf/sudokufx/testing/unit/viewmodel/MenuNewViewModelUTest.java
+++ src/test/java/fr/softsf/sudokufx/testing/unit/viewmodel/MenuNewViewModelUTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
 import javafx.application.Platform;
 import javafx.beans.binding.StringBinding;
 import javafx.concurrent.Task;
@@ -62,29 +61,40 @@ class MenuNewViewModelUTest {
 
     @Test
     void givenLocale_whenStringBindings_thenValuesMatchBeforeAndAfterChange() {
-        List<BindingCheck> bindings = List.of(
-                new BindingCheck(viewModel.maxiNewAccessibleTextProperty(), "menu.maxi.button.new.accessibility"),
-                new BindingCheck(viewModel.maxiNewTextProperty(), "menu.maxi.button.new.text"),
-                new BindingCheck(viewModel.newAccessibleTextProperty(), "menu.mini.button.new.accessibility"),
-                new BindingCheck(viewModel.maxiNewTooltipProperty(), "menu.maxi.button.new.accessibility"),
-                new BindingCheck(viewModel.newTooltipProperty(), "menu.mini.button.new.accessibility")
-        );
+        List<BindingCheck> bindings =
+                List.of(
+                        new BindingCheck(
+                                viewModel.maxiNewAccessibleTextProperty(),
+                                "menu.maxi.button.new.accessibility"),
+                        new BindingCheck(
+                                viewModel.maxiNewTextProperty(), "menu.maxi.button.new.text"),
+                        new BindingCheck(
+                                viewModel.newAccessibleTextProperty(),
+                                "menu.mini.button.new.accessibility"),
+                        new BindingCheck(
+                                viewModel.maxiNewTooltipProperty(),
+                                "menu.maxi.button.new.accessibility"),
+                        new BindingCheck(
+                                viewModel.newTooltipProperty(),
+                                "menu.mini.button.new.accessibility"));
         for (String locale : List.of("FR", "EN")) {
             I18n.INSTANCE.setLocaleBundle(locale);
-            bindings.forEach(b ->
-                    assertEquals(I18n.INSTANCE.getValue(b.i18nKey), b.binding.get())
-            );
+            bindings.forEach(b -> assertEquals(I18n.INSTANCE.getValue(b.i18nKey), b.binding.get()));
         }
     }
 
     @Test
-    void givenVersionService_whenCheckLatestVersion_thenIsUpToDateAndStatusMessageUpdated() throws Exception {
+    void givenVersionService_whenCheckLatestVersion_thenIsUpToDateAndStatusMessageUpdated()
+            throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
-        viewModel.statusMessageProperty().addListener((obs, oldVal, newVal) -> {
-            if ("Up to date".equals(newVal)) {
-                latch.countDown();
-            }
-        });
+        viewModel
+                .statusMessageProperty()
+                .addListener(
+                        (obs, oldVal, newVal) -> {
+                            if ("Up to date".equals(newVal)) {
+                                latch.countDown();
+                            }
+                        });
         assertTrue(latch.await(2, TimeUnit.SECONDS), "Timeout waiting for 'Up to date' message");
         assertTrue(viewModel.isUpToDateProperty().get());
         assertEquals("Up to date", viewModel.statusMessageProperty().get());
